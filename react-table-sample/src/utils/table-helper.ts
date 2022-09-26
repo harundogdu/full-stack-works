c6b8e384bd9b8d6485b42d913aa8e185ebe0e7ac
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-26 15:40:47 +0300

    feat(table-react) : table-helper created

---
+++ react-table-sample/src/utils/table-helper.ts
@@ -0,0 +1,34 @@
+export const TableHeader = [
+    {
+        title: 'Company Name',
+        dataIndex: 'companyName',
+    },
+    {
+        title: 'Contact Name',
+        dataIndex: 'contactName',
+    },
+    {
+        title: 'Contact Title',
+        dataIndex: 'contactTitle',
+    },
+    {
+        title: 'City',
+        dataIndex: 'address.city',
+    },
+    {
+        title: 'Region',
+        dataIndex: 'address.region',
+    },
+    {
+        title: 'Postal Code',
+        dataIndex: 'address.postalCode',
+    },
+    {
+        title: 'Country',
+        dataIndex: 'address.country',
+    },
+    {
+        title: 'Phone',
+        dataIndex: 'address.phone',
+    }
+]
