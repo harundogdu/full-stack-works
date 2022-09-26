99fae0af9632f0d929c1895884834573a8340055
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-26 16:46:09 +0300

    feat(react-table) : table pagination created

--- react-table-sample/src/containers/table/index.tsx
+++ react-table-sample/src/containers/table/index.tsx
@@ -1,8 +1,25 @@
+import { Button } from 'components';
+import React from 'react';
 import { ITable } from 'types/table';
 
-const Table = ({ columns, data }: ITable) => {
+const Table = ({ columns, data, itemDeleteClick }: ITable) => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const indexOfLastRow = currentPage * rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+  const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
+
+  const handlePageChange = (event: any, value: number) => {
+    setCurrentPage(value);
+  };
+
+  const handleRowsPerPageChange = (event: any) => {
+    setRowsPerPage(event.target.value);
+  };
+
   return (
-    <table className='w-9/12'>
+    <table className='w-full px-2'>
       <thead className='border-b-1 border-gray-900'>
         <tr className='text-left text-gray-500'>
           {columns.map((column, index) => (
@@ -10,13 +27,13 @@ const Table = ({ columns, data }: ITable) => {
               {column.title}
             </th>
           ))}
-         <th className='py-2 uppercase text-xs'>Actions</th>
+          <th className='py-2 uppercase text-xs'>Actions</th>
         </tr>
       </thead>
       <tbody className='divide-y divide-gray-200'>
-        {data.splice(0, 10).map((row, index) => (
+        {currentRows.map((row, index) => (
           <tr key={index} className='hover:bg-gray-100 text-sm'>
-            <td>{row.companyName}</td>
+            <td className='py-2'>{row.companyName}</td>
             <td>{row.contactName}</td>
             <td>{row.contactTitle}</td>
             <td>{row.address.city}</td>
@@ -25,17 +42,57 @@ const Table = ({ columns, data }: ITable) => {
             <td>{row.address.country}</td>
             <td>{row.address.phone}</td>
             <td>
-              <button className='px-2 py-1 text-white bg-red-500 rounded-md'>
-                Delete
-              </button>
+              <Button
+                text='Delete'
+                className='bg-red-500'
+                onClick={() => itemDeleteClick(row.id)}
+              />
             </td>
           </tr>
         ))}
       </tbody>
       <tfoot>
         <tr>
-          <td colSpan={9} className='py-2 text-center text-gray-500'>
-            Showing 1 to 10 of {data.length} entries
+          <td colSpan={columns.length + 1} className='py-4 text-xs'>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center justify-center'>
+                <span className='text-sm text-gray-500'>Rows per page:</span>
+                <select
+                  className='ml-2 text-sm text-gray-500'
+                  value={rowsPerPage}
+                  onChange={handleRowsPerPageChange}
+                >
+                  <option value={10}>10</option>
+                  <option value={15}>15</option>
+                  <option value={25}>25</option>
+                  <option value={50}>50</option>
+                  <option value={100}>100</option>
+                </select>
+              </div>
+              <div className='flex items-center justify-center'>
+                <span className='text-sm text-gray-500'>
+                  Page {currentPage} of {Math.ceil(data.length / rowsPerPage)}
+                </span>
+              </div>
+              <div className='flex items-center justify-center'>
+                <button
+                  className='px-2 py-1 text-white bg-blue-500 rounded-md'
+                  onClick={() => handlePageChange(null, currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </button>
+                <button
+                  className='ml-2 px-2 py-1 text-white bg-blue-500 rounded-md'
+                  onClick={() => handlePageChange(null, currentPage + 1)}
+                  disabled={
+                    currentPage === Math.ceil(data.length / rowsPerPage)
+                  }
+                >
+                  Next
+                </button>
+              </div>
+            </div>
           </td>
         </tr>
       </tfoot>
