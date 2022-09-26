278ffb9c17ca43a77c2143c92ede9b9c5ea63e49
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-26 16:45:55 +0300

    feat(react-table) : supplier context methods done

--- react-table-sample/src/contexts/suppliersContext.tsx
+++ react-table-sample/src/contexts/suppliersContext.tsx
@@ -1,19 +1,69 @@
 import React, { createContext, useContext } from 'react';
 import { getSuppliers } from 'services/supplierServices';
+import { ISuppliers } from 'types/suppliersType';
 
-const initialState: { suppliers: any[]; setSuppliers: any } = {
+const initialState = {
   suppliers: [],
-  setSuppliers: () => {}
+  setSuppliers: () => {},
+  loading: false,
+  setLoading: () => {},
+  currentSupplier: '',
+  setCurrentSupplier: () => {},
+  handleFilteredSuppliers: () => {},
+  filteredSuppliers: [],
+  handleRemoveAll: () => {},
+  handleDeleteCurrentItemClick: () => {},
 };
 
 const SuppliersContext = createContext(initialState);
 
 export const SuppliersProvider = ({ children }: any) => {
   const [suppliers, setSuppliers] = React.useState([]);
+  const [filteredSuppliers, setFilteredSuppliers] = React.useState(suppliers);
+  const [currentSupplier, setCurrentSupplier] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
+
+  const handleFilteredSuppliers = () => {
+    const filtered = suppliers.filter(
+      (supplier: ISuppliers) =>
+        supplier.companyName
+          .toLowerCase()
+          .includes(currentSupplier.toLowerCase()) ||
+        supplier.contactName
+          .toLowerCase()
+          .includes(currentSupplier.toLowerCase()) ||
+        supplier.contactTitle
+          .toLowerCase()
+          .includes(currentSupplier.toLowerCase()) ||
+        supplier.address.city
+          .toLowerCase()
+          .includes(currentSupplier.toLowerCase())
+    );
+    setFilteredSuppliers(filtered);
+  };
+
+  const handleRemoveAll = () => {
+    setFilteredSuppliers([]);
+  };
+
+  const handleDeleteCurrentItemClick = (id: number) => {
+    const filtered = filteredSuppliers.filter(
+      (supplier: ISuppliers) => supplier.id !== id
+    );
+    setFilteredSuppliers(filtered);
+  };
 
   const fetchSuppliers = async () => {
-    const data = await getSuppliers();
-    setSuppliers(data);
+    try {
+      setLoading(true);
+      const supplier = await getSuppliers();
+      setSuppliers(supplier);
+      setFilteredSuppliers(supplier);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   React.useEffect(() => {
@@ -21,11 +71,17 @@ export const SuppliersProvider = ({ children }: any) => {
   }, []);
 
   const contextValue = {
-    suppliers,
-    setSuppliers
+    loading,
+    currentSupplier,
+    setCurrentSupplier,
+    handleFilteredSuppliers,
+    filteredSuppliers,
+    handleRemoveAll,
+    handleDeleteCurrentItemClick
   };
 
   return (
+    // @ts-ignore
     <SuppliersContext.Provider value={contextValue}>
       {children}
     </SuppliersContext.Provider>
