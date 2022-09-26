311c0f9a87a4273a70c803ce2e5a58c0309d161f
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-26 15:41:17 +0300

    feat(table-react) : table used

--- react-table-sample/src/App.tsx
+++ react-table-sample/src/App.tsx
@@ -1,9 +1,13 @@
 import { Table } from 'containers';
+import { useSuppliers } from 'contexts/suppliersContext';
+import { TableHeader } from 'utils/table-helper';
 
 function App() {
+  const { suppliers, setSuppliers } = useSuppliers();
+
   return (
-    <div className='app'>
-      <Table />
+    <div className='flex items-center justify-center w-full h-full min-h-screen'>
+      <Table columns={TableHeader} data={suppliers} />
     </div>
   );
 }
