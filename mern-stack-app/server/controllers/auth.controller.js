743afdce6c1abe8991c753c65e01abc75e144305
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-08 20:03:59 +0300

    feat(mern-blog) : update method created in auth controller

--- mern-stack-app/server/controllers/auth.controller.js
+++ mern-stack-app/server/controllers/auth.controller.js
@@ -21,7 +21,19 @@ export default {
     }
   },
   // update user
-  updateUser: async (req, res) => {},
+  updateUser: async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const user = await userModel.findByIdAndUpdate(userId, req.body, {
+        new: true
+      });
+      if (!user) return res.status(404).json({ message: 'User not found' });
+
+      return res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
   // delete user
   deleteUser: async (req, res) => {
     try {
